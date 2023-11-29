14226e9268b80d252698e0499da339fe58079092
amsun1 <152207744+amsun1@users.noreply.github.com>
2023-11-29 11:29:57 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
+#include "PlaylistNode.h"
+
 using namespace std;
 
-
-int main()
-{
-
-
-  
-  return 0;
+void PrintMenu(const string playlistTitle) {
+   /* Type your code here */
+   
 }
+
+PlaylistNode* ExecuteMenu(char option, string playlistTitle, PlaylistNode* headNode) {
+   /* Type your code here */
+   
+}
+
+int main() {
+   /* Type your code here */
+   
+   return 0;
+}
+
