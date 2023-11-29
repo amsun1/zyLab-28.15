b660fa21030d7e853a02215428d7a87d0b411adc
bonitadavis <152207429+bonitadavis@users.noreply.github.com>
2023-11-29 15:23:42 -0600

    Update PlaylistNode.cpp
    Defined accessor functions and print function

--- PlaylistNode.cpp
+++ PlaylistNode.cpp
@@ -1,1 +1,23 @@
-/* Type your code here */
+#include "PlaylistNode.h"
+
+// Accessor functions
+string PlaylistNode::GetID() {return uniqueID;}
+string PlaylistNode::GetSongName() {return songName;}
+string PlaylistNode::GetArtistName() {return artistName;}
+int PlaylistNode::GetSongLength() {return songLength;}
+// TODO: PlaylistNode* PlaylistNode::GetNext() {}
+
+// TODO: Mutator functions
+void PlaylistNode::InsertAfter(PlaylistNode* nodePtr) {
+
+}
+void PlaylistNode::SetNext(PlaylistNode* nodePtr) {
+
+}
+
+void PrintPlaylistNode() {
+  cout << "Unique ID: " << uniqueID << endl;
+  cout << "Song Name: " << songName << endl;
+  cout << "Artist Name: " << artistName << endl;
+  cout << "Song Length (in seconds): " << songLength << endl;
+}
