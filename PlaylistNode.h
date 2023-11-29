d9d0c7921dca440b129628617360321175552652
bonitadavis <152207429+bonitadavis@users.noreply.github.com>
2023-11-29 15:16:19 -0600

    Update PlaylistNode.h
    Added private data members, public member functions, and default constructor

--- PlaylistNode.h
+++ PlaylistNode.h
@@ -1,1 +1,31 @@
-/* Type your code here */
+#ifndef PLAYLISTNODEH
+#define PLAYLISTNODEH
+
+#include <string>
+
+using namespace std;
+
+class PlaylistNode {
+  private:
+    string uniqueID;
+    string songName;
+    string artistName;
+    int songLength;
+    PlaylistNode* nextNodePtr;
+
+  public:
+    PlaylistNode();
+    // TODO: parameterized constructor
+
+    string GetID();
+    string GetSongName();
+    string GetArtistName();
+    int GetSongLength();
+    PlaylistNode* GetNext(); // TODO: check if return type is correct
+    void InsertAfter(PlaylistNode* nodePtr);
+    void SetNext(PlaylistNode* nodePtr);
+    void PrintPlaylistNode();
+
+};
+
+#endif
