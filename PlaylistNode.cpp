6095bac635e9c8038d957e473dafdd0a16d9c703
amsun1 <152207744+amsun1@users.noreply.github.com>
2023-11-30 21:42:47 -0600

    Update PlaylistNode.cpp
    added this-> to end void

--- PlaylistNode.cpp
+++ PlaylistNode.cpp
@@ -15,9 +15,18 @@ void PlaylistNode::SetNext(PlaylistNode* nodePtr) {
 
 }
 
-void PrintPlaylistNode() {
-  cout << "Unique ID: " << uniqueID << endl;
-  cout << "Song Name: " << songName << endl;
-  cout << "Artist Name: " << artistName << endl;
-  cout << "Song Length (in seconds): " << songLength << endl;
+//void PrintPlaylistNode() {
+  //cout << "Unique ID: " << uniqueID << endl;
+  //cout << "Song Name: " << songName << endl;
+  //cout << "Artist Name: " << artistName << endl;
+  //cout << "Song Length (in seconds): " << songLength << endl;
+
+  void PlaylistNode::PrintPlaylistNode() const {
+    cout << "Unique ID: " << this->uniqueID << endl;
+    cout << "Song Name: " << this->songName << endl;
+    cout << "Artist Name: " << this->artistName << endl;
+    cout << "Song Length (in seconds): " << this->songLength << endl;
+    cout << endl;
+}
+
 }
